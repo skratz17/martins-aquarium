4df13a7397ceff047146f939b4663dff4b42c85f
Jacob Eckert <jweckert17@gmail.com>
2020-07-14 10:31:58 -0500

    added image to fish data objects

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -1,12 +1,12 @@
 const fishCollection = [];
 
-const addFish = (name, species, length, location, ...diet) => {
-  fishCollection.push({ name, species, length, location, diet });
+const addFish = (name, species, length, location, image, ...diet) => {
+  fishCollection.push({ name, species, length, location, image, diet });
 }
 
-addFish('Sharky Guy', 'Great White Shark', 240, 'Pacific Ocean', 'Flesh');
-addFish('Martin (No Relation)', 'Marlin', 184, 'Caribbean Sea', 'Plankton');
-addFish('Katana', 'Swordfish', 120, 'Indian Ocean', 'Shrimp', 'Krill');
-addFish('BigBoi', 'Blue Whale', 1176, 'Percy Priest Lake', 'Krill', 'Plankton');
+addFish('Sharky Guy', 'Great White Shark', 240, 'Pacific Ocean', './assets/fish/greatWhiteShark.jpeg', 'Flesh');
+addFish('Martin (No Relation)', 'Marlin', 184, 'Caribbean Sea', './assets/fish/marlin.jpeg', 'Plankton');
+addFish('Katana', 'Swordfish', 120, 'Indian Ocean', 'Shrimp', './assets/fish/swordfish.jpeg', 'Krill');
+addFish('BigBoi', 'Blue Whale', 1176, 'Percy Priest Lake', 'Krill', './assets/fish/blueWhale.jpeg', 'Plankton');
 
 export default fishCollection;
